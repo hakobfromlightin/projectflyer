5dde6f834eae89da5c336702c8b3451927ec2ebf
hakobfromlightin <hakob93baghdasaryan@gmail.com>
2015-12-03 18:43:31 +0400

    flyer form and basic routing

---
+++ resources/views/flyers/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+    <h1>Selling your home?</h1>
+    <hr>
+    <form method="post" action="/flyers" enctype="multipart/form-data">
+        @include('flyers.form')
+    </form>
+@stop
